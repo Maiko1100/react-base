241335aeb283f4c675dcfd28c3985fc330fa64f6
Maiko1100 <maiko1100@gmail.com>
2017-01-20 18:36:06 +0100

    Login werkent

--- src/js/actions/userActions.js
+++ src/js/actions/userActions.js
@@ -1,29 +1,56 @@
 import axios from "axios";
-export function fetchUser(id) {
 
-    return function(dispatch) {
+
+export function fetchUser(username, password) {
+
+    return function (dispatch) {
         dispatch({type: "FETCH_USER", payload: ""})
-        axios.get("https://jsonplaceholder.typicode.com/users/"+id)
+        axios.post("http://www.loginapi.nl/api/users/authenticate")
             .then((response) => {
                 dispatch({type: "FETCH_USER_FULFILLED", payload: response.data})
             })
             .catch((err) => {
                 dispatch({type: "FETCH_USER_REJECTED", payload: err})
-            })
+            }), {
+            auth: {
+                username: username,
+                password: password
+            }
+        }
+    }
+}
+export function fetchUser2(username, password) {
+
+    return function (dispatch) {
+        dispatch({type: "FETCH_USER", payload: ""})
+        axios({
+            method: 'post',
+            url: 'http://www.loginapi.nl/users/authenticate',
+            auth: {
+                username: username,
+                password: password
+            }
+        }).then((response) => {
+            dispatch({type: "FETCH_USER_FULFILLED", payload: response.data})
+        })
+            .catch((err) => {
+                dispatch({type: "FETCH_USER_REJECTED", payload: err})
+            });
     }
 }
 
-export function fetchUsers() {
-  return function(dispatch) {
 
-    axios.get("https://jsonplaceholder.typicode.com/users")
-        .then((response) => {
-          dispatch({type: "FETCH_USERS_FULFILLED", payload: response.data})
-        })
-        .catch((err) => {
-          dispatch({type: "FETCH_USERS_REJECTED", payload: err})
-        })
-  }
+export function fetchUsers() {
+    return function (dispatch) {
+
+        axios.get("https://jsonplaceholder.typicode.com/users")
+            .then((response) => {
+                dispatch({type: "FETCH_USERS_FULFILLED", payload: response.data})
+            })
+            .catch((err) => {
+                dispatch({type: "FETCH_USERS_REJECTED", payload: err})
+            })
+    }
 }
 
 // export function setUserName(name, id) {
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,24 +1,25 @@
-import React from "react"
-import { connect } from "react-redux"
+import React, {Component}from "react"
+import {connect} from "react-redux"
 
-import { fetchUsers } from "../actions/userActions"
-import { fetchUser } from "../actions/userActions"
+import {fetchUsers} from "../actions/userActions"
+import {fetchUser} from "../actions/userActions"
+import {fetchUser2} from "../actions/userActions"
 import MDSpinner from "react-md-spinner";
 
 @connect((store) => {
-  return {
-    users: store.users.users,
-    user: store.users.user,
-    fetchedUsers: store.users.fetchedUsers,
-      fetchedUser: store.users.fetchedUser,
-      fetchingUser: store.users.fetchingUser
-  };
+    return {
+        users: store.users.users,
+        user: store.users.user,
+        fetchedUsers: store.users.fetchedUsers,
+        fetchedUser: store.users.fetchedUser,
+        fetchingUser: store.users.fetchingUser
+    };
 })
-export default class Layout extends React.Component {
+export default class Layout extends Component {
 
-  componentWillMount() {
+    componentWillMount() {
 
-  }
+    }
 
     fetchUsers() {
         this.props.dispatch(fetchUsers())
@@ -28,30 +29,43 @@ export default class Layout extends React.Component {
         this.props.dispatch(fetchUser(this.refs.id.value))
     }
 
-  render() {
-    const { users , fetchedUsers , fetchingUser} = this.props;
+    login() {
+        this.props.dispatch(fetchUser2(this.refs.username.value, this.refs.password.value))
+    }
 
 
-      const mappedUsers = users.map(user => <li key={user.id}>{user.name}</li>)
-      var spinner, spinner2;
+    render() {
+        const {users, fetchedUsers, fetchingUser} = this.props;
 
 
-      if(!fetchedUsers){
-          spinner = <MDSpinner/>
-      }
-      if(fetchingUser){
-          spinner2 = <MDSpinner/>
-      }
+        const mappedUsers = users.map(user => <li key={user.id}>{user.name}</li>)
+        var spinner, spinner2;
 
-    return <div>
-        <input ref="id" onChange={(e) => {this.fetchUser(); }} type="text"/>
 
-        <h1>Hier komt een naam:{spinner2} {this.props.user.name}</h1>
+        if (!fetchedUsers) {
+            spinner = <MDSpinner/>
+        }
+        if (fetchingUser) {
+            spinner2 = <MDSpinner/>
+        }
 
-        <button onClick={this.fetchUsers.bind(this)}>load users</button>
-        <br/>
-        {spinner}
-        <ul>{mappedUsers}</ul>
-    </div>
-  }
+        return <div>
+            <input ref="id" onChange={(e) => {
+                this.fetchUser();
+            }} type="text"/>
+
+            <h1>Hier komt een naam:{spinner2} {this.props.user.name}</h1>
+
+            <button onClick={this.fetchUsers.bind(this)}>load users</button>
+            <br/>
+            {spinner}
+            <ul>{mappedUsers}</ul>
+
+            <input ref="username" type="text" placeholder="Gebruikersnaam"/>
+            <input ref="password" type="text" placeholder="Wachtwoord"/>
+
+            <button onClick={this.login.bind(this)}>login</button>
+
+        </div>
+    }
 }
